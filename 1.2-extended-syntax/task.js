da3600b7fd8eed5949c07a39fcb41d55f1756827
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2020-10-06 16:26:30 +0300

    Changed task second

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,48 @@
+"use sctrict";
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+   let D =  b**b - 4*a*c;
+
+   if ( D > 0 ) {
+   let x1 = (-b + Math.pow(D))/(2*a),
+    x2 = (-b - Math.pow(D))/(2*a);
+    console.log('x1 = '+x1+'   x2 = '+x2);
+   
+  } else if ( D == 0 ) {
+    x1 = (-b)/(2*a);
+    console.log('x = '+x1);
+    
+  } else if ( D < 0 ) {
+   console.log('Решений нет, так как дискриминант меньше нуля.');
+  }
+    return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+  let mark = [5, 4, 4, 4, 5];
+ 
+  if (mark.length == 0 ) {
+    return 0;
+  }
+  else if (mark.length > 5) {
+    mark.splice(mark[i]);
+    console.log("Количество оценок больше 5!")
+ }
+ 
+  let sum = 0;
+  for (let i = 0; i < mark.length; i++) {
+	  sum = sum + mark[i];
+  }
+  let averageMark = sum / mark.length
+    return averageMark;   
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+  let name;
+  const year = newDate().getFullYear();
+  if (year >= 2002) {
+    console.log(`Не желаете ли олд-фэшн, ${name}?`);
+  } else {
+    console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+  }
+     return year;
 }
