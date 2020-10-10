139ee182dd34a993bfa584db8501517a6fb17d7e
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2020-10-10 18:49:24 +0300

    Changed task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,8 +3,8 @@ function getResult(a,b,c){
    let D =  b**b - 4*a*c;
 
    if ( D > 0 ) {
-   let x1 = (-b + Math.pow(D))/(2*a),
-    x2 = (-b - Math.pow(D))/(2*a);
+   let x1 = (-b + Math.sqrt(D))/(2*a),
+    x2 = (-b - Math.sqrt(D))/(2*a);
     console.log('x1 = '+x1+'   x2 = '+x2);
    
   } else if ( D == 0 ) {
@@ -14,35 +14,37 @@ function getResult(a,b,c){
   } else if ( D < 0 ) {
    console.log('Решений нет, так как дискриминант меньше нуля.');
   }
-    return x;
+    return [x1, x2];
 }
 
 function getAverageMark(marks) {
-  let mark = [5, 4, 4, 4, 5];
  
-  if (mark.length == 0 ) {
+  if (marks.length === 0 ) {
     return 0;
   }
-  else if (mark.length > 5) {
-    mark.splice(mark[i]);
+  else if (marks.length > 5) {
+    marks.splice(5, 1);
     console.log("Количество оценок больше 5!")
  }
  
   let sum = 0;
   for (let i = 0; i < mark.length; i++) {
-	  sum = sum + mark[i];
+	  sum = sum + marks[i];
   }
-  let averageMark = sum / mark.length
-    return averageMark;   
+  return sum / marks.length;
+    
 }
 
 function askDrink(name, dateOfBirthday) {
-  let name;
-  const year = newDate().getFullYear();
-  if (year >= 2002) {
+  dateOfBirthday = new Date(dateOfBirthday);
+  let now =  new Date();
+  age = now.getFullYear() - dateOfBirthday.getFullYear();
+  if (age > 18) 
     console.log(`Не желаете ли олд-фэшн, ${name}?`);
-  } else {
+  else (age < 18)
     console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
-  }
-     return year;
+  
+     return;
 }
+    
+      
