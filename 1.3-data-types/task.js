1cf4559e8198033d1e579f1812f1580491825ad3
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2020-10-15 22:35:12 +0300

    Modified task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,14 +1,17 @@
 "use sctrict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  
-
-} 
+  let credit = amount - contribution;
+  let months = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+  let P = (percent / 100)/ 12;
+  let payment = credit*(P+P/(Math.pow((1+P),months)-1));
+  let totalAmount = contribution + (payment * months);
+  totalAmount = totalAmount.toFixed(2);
     return totalAmount;
 }
 
 function getGreeting(name) {
  let greeting;
-if (name === 'null' || name === 'undefined' || name === '') {
+if (!name) {
   greeting = "Привет, мир! Меня зовут Аноним";
 }
 else {
