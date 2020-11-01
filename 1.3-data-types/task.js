15de2d8b03c24dbdb4b6387fd91ef70382f565fe
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2020-11-02 00:20:41 +0300

    changed task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -5,22 +5,23 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let P = (percent / 100)/ 12;
   let payment = (credit*(P+P/(Math.pow((1+P),months)-1)));
   let totalAmount = (contribution + (payment * months));
-  totalAmount = totalAmount.toFixed(2);
-  if (Number.isNaN(percent)) {
-    return 'Проценты введены неверно'
+  if (isNaN(percent)) {
+    return 'Проценты введены неверно';  
   }
-    return totalAmount;
-
-    
+  if (isNaN(contribution)) {    
+    return 'Параметр вклад введен неверно';
+  }
+  if (isNaN(amount)) {
+    return 'Параметр amount введен неверно ';
+   }
+   totalAmount = totalAmount.toFixed(2);
+   totalAmount = Number(totalAmount);
+  return totalAmount;
 }
+  
 
 function getGreeting(name) {
- let greeting;
-if (name === 'null' || name === '' || name === 'undefined') {
-  greeting = "Привет, мир! Меня зовут Аноним";
-}
-else {
-  greeting = `Привет, мир! Меня зовут ${name}`
-}
+ let greeting = "Привет, мир! Меня зовут";
+ greeting = (name === "null" || name === "undefined" || name === "" || name == false) ? greeting + ' Аноним' : greeting + ` ${name}`;
    return greeting;
 }
