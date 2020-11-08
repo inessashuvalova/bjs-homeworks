21b0e587183d9598175b7de6f1ffee950bb2a89f
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2020-11-08 21:11:42 +0300

    changed functions.js

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -1,6 +1,33 @@
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
-function getNames(weapon) {
-   (weapons.find(weapon)) 
-      return (weapon);
+class Weapon {
+   constructor(name, durability) {
+       this.name = name;
+       this.durability = durability;
+
+function getNames(name) {
+   (weapons.find(this.name)) 
+      return name;
    };
+
+function getCountReliableWeapons(durability) {
+      (weapons.find(this.durability)) 
+         return (weapon);
+      };
+
+function hasReliableWeapons(durability) {
+      (weapons.some(this.durability)) 
+         return quantity > durability;
+      };
+
+function getReliableWeaponsNames(durability) { 
+        return name > durability;
+      };
+      
+// function getTotalDamage(weapon) {
+//       (weapons.find(weapon)) 
+//         return (weapon);
+//       };
+
+   }
+}
