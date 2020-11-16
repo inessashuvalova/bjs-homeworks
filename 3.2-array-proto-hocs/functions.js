72dcd896dce283b3477d33867a1ebdfbeba26641
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2020-11-17 01:20:55 +0300

    changed functions.js

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -22,4 +22,42 @@ function getTotalDamage () {
     return weapons.reduce((totalDamage, weapon) => totalDamage + weapon.attack, 0);
 }
       
-   
+// Задача 2
+
+  function sleep(milliseconds) {
+    let e = new Date().getTime() + milliseconds;
+    while (new Date().getTime() <= e) {}
+  }
+  function sum(...args) {
+    sleep(100); // delay 100ms
+    return args.reduce((sum, arg) => {
+      return sum += +arg;
+    }, 0);
+  }
+
+  function compareArrays(arr1, arr2) {
+    if (arr1 && arr2 && arr1.every((value, index) => value === arr2[index])) { 
+      return true; 
+    }
+    else {
+      return false; 
+    }
+  }
+
+  function memorize(fn, limit) {
+    const memory = [];
+    memory.length = 0;
+    return function memorizeReturn (...theArgs) {
+
+      let searchArgs = memory.find(item => compareArrays(theArgs, item.args));
+      if (searchArgs != undefined) {
+        return searchArgs.result;
+      }
+  
+      let resultFn = fn(...theArgs);
+      if (memory.unshift({args: theArgs, result: resultFn}) > limit) {
+        memory.length = limit;
+      };
+      return resultFn;
+    }
+  }
